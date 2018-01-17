c0cd477951a371da93298d3fa5ab25a89a6e20ec
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-01-17 12:28:36 -0400

    controlador  de usuarios duplicados

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,37 +8,56 @@ function home(req, res) {
         message: 'Hola Mundo desde el servidor'
     });
 }
+
 function pruebas(req, res) {
     res.status(200).send({
         message: 'pruebas con nodejs'
     });
 }
+
 function saveUser(req, res) {
     const params = req.body;
     let user = new User();
 
     if (params.name && params.surname &&
-        params.nick && params.email && params.password){
+        params.nick && params.email && params.password) {
         user.name = params.name;
         user.surname = params.surname;
-        user.nick = params.nick;
-        user.email = params.email;
+        user.nick = params.nick.toLowerCase();
+        user.email = params.email.toLowerCase();
         user.password = params.password;
         user.role = 'ROLE_USER';
         user.image = null;
 
-        bcrypt.hash(params.password, null, null, (err, hash) =>{
-            user.password = hash;
-            user.save((err, userStored) =>{
-                if (err) return res.status(500).send({message: 'Error al guardar usuario'});
-                if (userStored){
-                    res.status(200).send({user: userStored, message: 'Usuario registrado correctamente'});
-                }else{
-                    res.status(404).send({message: 'No se ha registrado el usuario'});
-                }
-            })
-        })
-    }else {
+        // controlar usuarios duplicados
+        // buscar usuarios duplicados
+        User.find({
+            $or: [
+                {email: user.email},
+                {nick: user.nick}
+            ]
+        }).exec((err, users) => {
+            // console.log(users);
+            if (err) return res.status(500).send({message: 'Error en la peticion de usuario'});
+            if (users && users.length >= 1) {
+                return res.status(200).send({message: 'El usuario que intenta registrar ya existe'});
+            } else {
+                bcrypt.hash(params.password, null, null, (err, hash) => {
+                    user.password = hash;
+                    user.save((err, userStored) => {
+                        if (err) return res.status(500).send({message: 'Error al guardar usuario'});
+                        if (userStored) {
+                            res.status(200).send({user: userStored, message: 'Usuario registrado correctamente'});
+                        } else {
+                            res.status(404).send({message: 'No se ha registrado el usuario'});
+                        }
+                    })
+                })
+            }
+        });
+
+
+    } else {
         res.status(200).send({
             message: 'Envia todos ls campos necesarios!!'
         });
