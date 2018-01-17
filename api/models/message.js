ae8b69d2c21cfcc8f7f52f9d6c4696f4fece7e37
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-01-17 11:26:55 -0400

    modelo Message creado

---
+++ api/models/message.js
@@ -0,0 +1,10 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const MessageSchema = Schema({
+    text: String,
+    create_at: String,
+    emitter: {type: Schema.ObjectId, ref: 'User'},
+    receiver: {type: Schema.ObjectId, ref: 'User'}
+});
+module.exports = mongoose.model('Message', MessageSchema);
