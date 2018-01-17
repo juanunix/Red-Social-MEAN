8020cb02cc762137af923d914a6db8a4b7beb28d
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-01-17 11:20:58 -0400

    modelo Publication creado

---
+++ api/models/publication.js
@@ -0,0 +1,10 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const PublicationSchema = Schema({
+    text: String,
+    file: String,
+    create_at: String,
+    user: {type: Schema.ObjectId, ref: 'User'}
+});
+module.exports = mongoose.model('Publication', PublicationSchema);
