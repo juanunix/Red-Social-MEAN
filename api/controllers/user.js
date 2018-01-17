809b2ac1246e1e64d46fe51c57b40d9ee07300be
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-01-17 12:12:04 -0400

    Registro de usuario cifrando contrasena

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const User = require('../models/user');
+const bcrypt = require('bcrypt-nodejs');
 
 //rutas
 function home(req, res) {
@@ -12,8 +13,40 @@ function pruebas(req, res) {
         message: 'pruebas con nodejs'
     });
 }
+function saveUser(req, res) {
+    const params = req.body;
+    let user = new User();
+
+    if (params.name && params.surname &&
+        params.nick && params.email && params.password){
+        user.name = params.name;
+        user.surname = params.surname;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.password = params.password;
+        user.role = 'ROLE_USER';
+        user.image = null;
+
+        bcrypt.hash(params.password, null, null, (err, hash) =>{
+            user.password = hash;
+            user.save((err, userStored) =>{
+                if (err) return res.status(500).send({message: 'Error al guardar usuario'});
+                if (userStored){
+                    res.status(200).send({user: userStored, message: 'Usuario registrado correctamente'});
+                }else{
+                    res.status(404).send({message: 'No se ha registrado el usuario'});
+                }
+            })
+        })
+    }else {
+        res.status(200).send({
+            message: 'Envia todos ls campos necesarios!!'
+        });
+    }
+}
 
 module.exports = {
     home,
-    pruebas
+    pruebas,
+    saveUser
 };
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const express = require('express');
 const UserController = require('../controllers/user');
 const api = express.Router();
 
-api.get('/home', UserController.home);
 api.get('/pruebas', UserController.pruebas);
+api.get('/home', UserController.home);
+api.post('/register', UserController.saveUser);
 module.exports = api;
